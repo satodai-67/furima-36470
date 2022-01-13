9c82d98a26fb27b3ea080071b3db5005250ff173
satodai-67 <baseballsprit1014@icloud.com>
2022-01-13 16:16:06 +0900

    ActiveHashとバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,6 @@ class ItemsController < ApplicationController
   def index
   end
 
-
-
-
-
-  
   private
   def item_params
     params.require(:item).permit(:item_name, :item_explanation, :category_id, :state_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :shipping_fee
+    belongs_to :state
+    belongs_to :prefecture
+    belongs_to :shipping_day
     belongs_to :user
     #has_one :purchase
 
     has_one_attached :image
+
+    validates :image, presence: true
+    validates :item_name, presence: true
+    validates :item_explanation, presence: true
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :state_id, numericality: { other_than: 1 } 
+    validates :shipping_fee_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :shipping_day_id, numericality: { other_than: 1 } 
+    validates :price, presence: true
 end
