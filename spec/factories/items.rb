e1654e68e56559c8a50de0a0824f74638ec86288
satodai-67 <baseballsprit1014@icloud.com>
2022-01-15 19:46:53 +0900

    商品一覧表示機能実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
-    image {'test_image.png'}
-    item_name {'test'}
-    item_explanation {'testtest'}
+    image { 'test_image.png' }
+    item_name { 'test' }
+    item_explanation { 'testtest' }
     category_id { 2 }
     state_id { 3 }
     shipping_fee_id { 2 }
@@ -14,7 +14,6 @@ FactoryBot.define do
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
-    factory :user do
-      nickname              {Faker::Name.name}
-      email                 {'free@gmail.com'}
-      password              {'aaaa0000'}
-      password_confirmation {password}
-      first_name            {"佐藤"}
-      last_name             {"吾郎"}
-      firstkana_name        {"サトウ"}
-      lastkana_name         {"ゴロウ"}
-      birth_date            {Faker::Date.birthday(min_age: 18, max_age: 65)}
-    end
+  factory :user do
+    nickname              { Faker::Name.name }
+    email                 { 'free@gmail.com' }
+    password              { 'aaaa0000' }
+    password_confirmation { password }
+    first_name            { '佐藤' }
+    last_name             { '吾郎' }
+    firstkana_name        { 'サトウ' }
+    lastkana_name         { 'ゴロウ' }
+    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) }
+  end
 end
