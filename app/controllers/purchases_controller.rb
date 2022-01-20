40a632a93d1a02e71b9cf1d383ef70971050d880
satodai-67 <baseballsprit1014@icloud.com>
2022-01-20 20:23:53 +0900

    修正➂

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -30,7 +30,7 @@ class PurchasesController < ApplicationController
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: @item[:price],
+      amount: @item.price,
       card: purchase_params[:token],
       currency: 'jpy'
     )
